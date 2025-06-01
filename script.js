99926c0c5a661ac928a5d589d3aedd7f2768dfb9
Eliot JOUAN <eliotjouan@MacBook-Pro-Eliot.local>
2025-06-01 14:52:37 +0200

    Mise en ligne de mon portfolio

---
+++ script.js
@@ -0,0 +1,36 @@
+const output = document.getElementById('output');
+const cursor = document.getElementById('cursor');
+
+const messages = [
+    "Hello !",
+    "   Bienvenue sur mon site ! :)",
+    "   Chargement des données...",
+    "   Vous êtes connecté en tant qu'invité",
+    "   Veuillez patienter...",
+];
+
+let currentMessage = 0;
+
+function typeMessage(message, index) {
+    if (index < message.length) {
+        output.textContent += message.charAt(index);
+        setTimeout(() => typeMessage(message, index + 1), 50);//50 = seonde pour taper les phrases
+    } else {
+        currentMessage++;
+        if (currentMessage < messages.length) {
+            setTimeout(() => {
+                output.textContent += '\n';
+                typeMessage(messages[currentMessage], 0);
+            }, 800);
+        } else {
+            // Redirection après la dernière ligne
+            setTimeout(() => {
+                cursor.style.display ='none'
+                window.location.href = 'projets.html'; // URL de page d'accueil
+            }, 1500);// 1500 = seconde pour allé sur page acceuil
+        }
+    }
+}
+
+// Démarrer l'écriture du premier message
+typeMessage(messages[currentMessage], 0);
